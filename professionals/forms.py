748be84ad398d2a50702b04c400cc05109af488b
Edward Hanson <ehanson787@gmail.com>
2023-08-31 18:20:49 +0000

    signup template

--- professionals/forms.py
+++ professionals/forms.py
@@ -4,4 +4,4 @@ from .models import Profs
 class ProfForm(forms.ModelForm):
     class Meta:
         model = Profs
-        fields= ('occupation','name','title','institution','profile_pic','telephone',)
+        fields= ('occupation','name','title','institution','details','profile_pic','telephone',)
